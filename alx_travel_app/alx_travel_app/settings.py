75783a1ae58ea3b8949fecbc1f41c27473fb5d35
lawalgodwin <godwinl200@gmail.com>
2025-02-05 16:45:46 +0100

    Set Up Chapa API Credentials

--- alx_travel_app/alx_travel_app/settings.py
+++ alx_travel_app/alx_travel_app/settings.py
@@ -12,10 +12,17 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 
+import environ
+import os
+
+env = environ.Env(
+    # set casting, default value
+    DEBUG=(bool, False)
+)
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
@@ -25,8 +32,7 @@ SECRET_KEY = 'django-insecure-e3&jjg+*t+bu1tt)^9(q4i6u%uj!k4n^phrc!2()bm0ly%5+ju
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["booking-systems.onrender.com"]
-
+ALLOWED_HOSTS = ["booking-systems.onrender.com", "localhost", "127.0.0.1", "*"]
 
 # Application definition
 
@@ -192,3 +198,6 @@ REDOC_SETTINGS = {
     'EXPAND_RESPONSES': 'all',
     'PATH_IN_MIDDLE': False,
 }
+
+CHAPA_SECRET_KEY = (env('CHAPA_SECRET_KEY'))
+
